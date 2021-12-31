c99d657040e4e0d891ca4fc0bb14c7472df74d6c
Lina Vallejo <linav92@gmail.com>
2021-12-31 20:24:37 -0300

    Delete import swal from main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import swal from 'sweetalert';
-
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App, swal),
+  render: h => h(App),
 }).$mount('#app')
